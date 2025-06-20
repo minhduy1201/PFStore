089826ecb896534ad9e6e6d9a2f59c30053851cf
thaituongan <21130268@st.hcmuaf.edu.vn>
2025-06-20 10:09:45 +0700

    ok

--- src/servers/connection.js
+++ src/servers/connection.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Alert } from 'react-native';
 
 
-const API_BASE_URL = 'https://d5cd-115-74-23-79.ngrok-free.app/api';
+const API_BASE_URL = 'https://1368-115-79-219-34.ngrok-free.app/api';
 
 // Tạo một instance của axios
 export const api = axios.create({
