2ced62a1f5422d3031a58b5d63deac3d886b411f
nttduongg <21130032@st.hcmuaf.edu.vn>
2025-06-22 14:44:47 +0700

    dat hang

--- src/servers/connection.js
+++ src/servers/connection.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Alert } from "react-native";
-const API_BASE_URL = "https://90a7-118-71-92-115.ngrok-free.app/api";
+const API_BASE_URL = "https://f8f5-14-241-170-199.ngrok-free.app/api";
 
 // Tạo một instance của axios
 export const api = axios.create({
