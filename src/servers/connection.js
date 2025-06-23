b608e38d882d626e58be348d9f47a49d4db121fa
minhduy1201 <21130333@st.hcmuaf.edu.vn>
2025-06-24 03:19:26 +0700

    .

--- src/servers/connection.js
+++ src/servers/connection.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Alert } from "react-native";
-const API_BASE_URL = "https://90a7-118-71-92-115.ngrok-free.app/api";
+const API_BASE_URL = "https://b442-125-235-175-141.ngrok-free.app/api";
 
 // Tạo một instance của axios
 export const api = axios.create({
